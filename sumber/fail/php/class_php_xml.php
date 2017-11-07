4dc286203455a17d3fcd13ca71b3c0a6222f3f7d
amin007 <helmi007@yahoo.com>
2017-11-07 16:07:07 +0800

    Add files via upload

---
+++ sumber/fail/php/class_php_xml.php
@@ -0,0 +1,27 @@
+<?php
+
+function array_to_xml($array, &$xml_user_info) 
+{
+	# buat pecahan tatasusunan
+	foreach($array as $key => $value)
+	{
+		if(is_array($value)) 
+		{
+			if(!is_numeric($key))
+			{
+				$subnode = $xml_user_info->addChild("$key");
+				array_to_xml($value, $subnode);
+			}
+			else
+			{
+				//$subnode = $xml_user_info->addChild("item$key");
+				$subnode = $xml_user_info->addChild("item");
+				array_to_xml($value, $subnode);
+			}
+		}
+		else
+		{
+			$xml_user_info->addChild("$key",htmlspecialchars("$value"));
+		}
+	}
+}
